6f9c99f08f30875f4d0a19ff4891dc433c534388
Abrar-0 <abrarzawad222@gmail.com>
2024-04-10 22:17:11 +0600

    starting

---
+++ src/__steps.js
@@ -0,0 +1,6 @@
+// 1. visit console firebase
+// create project
+// Register app
+// install firebase
+// add config file --> firebase.init.js
+// Danger: do not publish or make firebase config file to public those to github
