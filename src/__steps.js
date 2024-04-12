87a9d78f7f97e787da0c3eec8c9e449d51e887cf
Abrar-0 <abrarzawad222@gmail.com>
2024-04-12 21:29:06 +0600

    wowow

--- src/__steps.js
+++ src/__steps.js
@@ -1,6 +1,13 @@
-// 1. visit console firebase
+// visit console firebase
 // create project
 // Register app
 // install firebase
 // add config file --> firebase.init.js
 // Danger: do not publish or make firebase config file to public those to github
+// Go to docs > build  > authentication > web > get started
+// Export app from firebase init  -> export default app
+// Use it in login.jsx -> getAuth(app)
+// import google auth provider in login.jsx and create a new provider
+// use Signinwithpopup and pass auth and provider
+// activate sign in method (fb,google,twitter)
+// 
