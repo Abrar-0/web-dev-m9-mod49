1dc2b6f2f2775824f2c55ff286ea8d05a5a0e46e
Abrar Zawad <abrarzawad222@gmail.com>
2024-04-14 00:50:02 +0600

    done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   GithubAuthProvider,
   GoogleAuthProvider,
@@ -29,21 +29,22 @@ const Login = () => {
     signInWithPopup(auth, providerGit)
       .then((result) => {
         // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-        const credential = GithubAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
+        // const credential = GithubAuthProvider.credentialFromResult(result);
+        // const token = credential.accessToken;
 
         // The signed-in user info.
         const user = result.user;
         setUser(user)
       })
       .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = GithubAuthProvider.credentialFromError(error);
+        // // Handle Errors here.
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        // // The email of the user's account used.
+        // const email = error.customData.email;
+        // // The AuthCredential type that was used.
+        // const credential = GithubAuthProvider.credentialFromError(error);
+        console.log(error.message)
         // ...
       });
   };
